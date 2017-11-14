6bc221b598452857150f24e48d0080638fc18395
Tim Savage <tim@savage.company>
2017-11-15 01:37:01 +1100

    Define the basic operation decorator

--- odinweb3/decorators.py
+++ odinweb3/decorators.py
@@ -1,6 +1,20 @@
-from typing import Callable, Union
+from typing import Callable, Union, List
+
+from .constants import Method
 
 PathTypes = Union[str]
+
+
+def operation(path: PathTypes, method: Method=Method.Get, operation_id: str=None,
+              summary: str=None, tags: List[str]=None) -> Callable:
+    """
+    Decorator for defining an API operation. Usually one of the helpers (listing, detail, update, delete) would be
+    used in place of this Operation decorator.
+    """
+    def inner(callback):
+        return Operation(callback, path, method, operation_id, summary, tags)
+    return inner
+
 
 
 class Operation:
@@ -27,3 +41,6 @@ class Operation:
         return inner(func) if func else inner
 
     def __init__(self, callback: Callable, path: PathTypes, ):
+        pass
+
+
