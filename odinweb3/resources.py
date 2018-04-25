405b2e861d62ae92c49d72eafea24a4240f031d5
Tim Savage <tim@savage.company>
2018-04-26 01:39:35 +1000

    Drop listing resource in favour of headers

--- odinweb3/resources.py
+++ odinweb3/resources.py
@@ -7,44 +7,8 @@ Common resources for use in APIs.
 """
 import odin
 
-from typing import Any
-from odin.fields import Field
-
 from .constants import Status
-
-
-class AnyField(Field):
-    """
-    Any value.
-    """
-    def to_python(self, value):
-        return value
-
-
-class Listing(odin.Resource):
-    """
-    Response for listing results. THis includes offset and count support for
-    paging etc.
-
-    """
-    class Meta:
-        namespace = None
-
-    results = odin.ArrayField(
-        help_text="List of resources."
-    )
-    limit = odin.IntegerField(
-        null=True,
-        help_text="Limit or page size of the result set"
-    )
-    offset = odin.IntegerField(
-        default=0,
-        help_text="Offset within the result set."
-    )
-    total_count = odin.IntegerField(
-        null=True,
-        help_text="The total number of items in the result set."
-    )
+from .typing import OpenApiAny
 
 
 class Error(odin.Resource):
@@ -61,7 +25,7 @@ class Error(odin.Resource):
 
     @classmethod
     def from_status(cls, status: Status, code_index: int=0, message: str=None,
-                    developer_message: str=None, meta: Any=None) -> 'Error':
+                    developer_message: str=None, meta: OpenApiAny=None) -> 'Error':
         """
         Automatically build an HTTP response from the HTTP Status code.
         
@@ -80,21 +44,21 @@ class Error(odin.Resource):
             meta
         )
 
-    status = odin.IntegerField(
+    status = odin.Integer(
         help_text="HTTP status code of the response."
     )
-    code = odin.IntegerField(
+    code = odin.Integer(
         help_text="Custom application specific error code that references into "
                   "the application."
     )
-    message = odin.StringField(
+    message = odin.String(
         help_text="A message that can be displayed to an end user"
     )
-    developer_message = odin.StringField(
+    developer_message = odin.String(
         null=True,
         help_text="An error message suitable for the application developer"
     )
-    meta = AnyField(
+    meta = odin.AnyValue(
         null=True,
         help_text="Additional meta information that can help solve errors."
     )
