2a575987a08dbb78c9bac4278e8423d5acae8921
Tim Savage <tim@savage.company>
2018-04-26 01:54:28 +1000

    Applied typing

--- odinweb3/utils.py
+++ odinweb3/utils.py
@@ -7,12 +7,11 @@ Utility functions used to perform common operations.
 """
 import os
 import base64
-import itertools
 
-from typing import Iterable
+from typing import Iterable, Callable
 
 
-def token(bit_depth: int=64, encoder=base64.b32encode) -> str:
+def token(bit_depth: int=64, encoder: Callable[[str], bytes]=base64.b32encode) -> str:
     """
     Generate a random token of a certain bit depth and strip any padding.
     """
@@ -23,24 +22,23 @@ def token(bit_depth: int=64, encoder=base64.b32encode) -> str:
     raise ValueError("Bit depth must be a multiple of 8")
 
 
-def dict_filter_update(base: dict, updates: dict):
+def dict_filter(*args: dict, base: dict=None) -> dict:
     """
-    Update dict with None values filtered out.
-    """
-    base.update((k, v) for k, v in updates.items() if v is not None)
+    Merge values from multiple dictionaries into a single dictionary while
+    filtering out ``None`` values.
 
+    Use the *base* argument to provide a base dictionary (this dict will not
+    be ``None`` filtered). This is to handle dictionaries that have already
+    been filtered.
 
-def dict_filter(*args: dict, **kwargs) -> dict:
     """
-    Merge all values into a single dict with all None values removed.
-    """
-    result = {}
-    for arg in itertools.chain(args, (kwargs,)):
-        dict_filter_update(result, arg)
+    result = base or {}
+    for arg in args:
+        result.update(i for i in arg.items() if i[1] is not None)
     return result
 
 
-def sort_by_priority(iterable: Iterable, reverse: bool=False, default_priority: int=10):
+def sort_by_priority(iterable: Iterable, reverse: bool=False, default_priority: int=10) -> list:
     """
     Return a list or objects sorted by a priority value.
     """
