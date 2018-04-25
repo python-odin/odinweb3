8b2a9ee2540b9124092223bd08b019e9c7df855d
Tim Savage <tim@savage.company>
2018-04-26 01:54:55 +1000

    Added some OpenApi type constants

--- odinweb3/typing.py
+++ odinweb3/typing.py
@@ -1,4 +1,14 @@
-from typing import Any, Dict, Callable, Optional
+from typing import Any, Dict, Callable, Optional, Union, List
+
+OpenApiAny = Optional[Union[str, int, float, bool, List['EncodedType'], Dict[str, 'EncodedType']]]
+"""
+Any types supported by Open API (used as to_openapi response type) 
+"""
+
+OpenApiObject = Dict[str, OpenApiAny]
+"""
+The OpenAPI Object type.
+"""
 
 # Mapping of strings (commonly used for headers)
 StringMap = Dict[str, str]
